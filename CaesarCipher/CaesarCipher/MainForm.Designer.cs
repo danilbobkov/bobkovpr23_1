2196d6a5213a7e211fcc8d3b69d58a5dbb9b27cc
danilbobkov <danil.rend.bobkov@gmail.com>
2025-11-03 08:49:54 +0300

    Add files via upload

---
+++ CaesarCipher/CaesarCipher/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CaesarCipher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shiftComboBox = new System.Windows.Forms.ComboBox();
+            this.languageComboBox = new System.Windows.Forms.ComboBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shiftComboBox
+            // 
+            this.shiftComboBox.FormattingEnabled = true;
+            this.shiftComboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.shiftComboBox.Location = new System.Drawing.Point(28, 422);
+            this.shiftComboBox.Name = "shiftComboBox";
+            this.shiftComboBox.Size = new System.Drawing.Size(121, 21);
+            this.shiftComboBox.TabIndex = 0;
+            // 
+            // languageComboBox
+            // 
+            this.languageComboBox.FormattingEnabled = true;
+            this.languageComboBox.Items.AddRange(new object[] {
+            "Русский",
+            "English"});
+            this.languageComboBox.Location = new System.Drawing.Point(180, 421);
+            this.languageComboBox.Name = "languageComboBox";
+            this.languageComboBox.Size = new System.Drawing.Size(121, 21);
+            this.languageComboBox.TabIndex = 1;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(124, 27);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(678, 61);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(124, 106);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(678, 100);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(428, 421);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Зашифровать";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(556, 422);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Расшифровать";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(679, 419);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 467);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.languageComboBox);
+            this.Controls.Add(this.shiftComboBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox shiftComboBox;
+        private System.Windows.Forms.ComboBox languageComboBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ CaesarCipher/CaesarCipher/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CaesarCipher
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            SetupComboBoxes();
+        }
+
+        private void SetupComboBoxes()
+        {
+            // Настройка комбобокса для выбора языка
+            languageComboBox.Items.Clear();
+            languageComboBox.Items.AddRange(new[] { "Русский", "English" });
+            languageComboBox.SelectedIndex = 0;
+
+            // Настройка комбобокса для выбора сдвига
+            shiftComboBox.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                shiftComboBox.Items.Add(i);
+            }
+            shiftComboBox.SelectedIndex = 2; // Выбор сдвига 3 по умолчанию
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            ProcessText(true);
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            ProcessText(false);
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+        }
+
+        private void ProcessText(bool encrypt)
+        {
+            // Проверка ввода
+            if (string.IsNullOrWhiteSpace(inputTextBox.Text))
+            {
+                MessageBox.Show("Введите текст для обработки");
+                return;
+            }
+
+            if (shiftComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите величину сдвига");
+                return;
+            }
+
+            // Получение параметров
+            int shift = (int)shiftComboBox.SelectedItem;
+            bool isRussian = languageComboBox.SelectedIndex == 0;
+            string alphabet = isRussian ? "абвгдежзийклмнопрстуфхцчшщъыьэюя" : "abcdefghijklmnopqrstuvwxyz";
+
+            // Обработка текста
+            StringBuilder result = new StringBuilder();
+            foreach (char c in inputTextBox.Text)
+            {
+                if (char.IsLetter(c))
+                {
+                    bool isUpper = char.IsUpper(c);
+                    char lowerChar = char.ToLower(c);
+
+                    int index = alphabet.IndexOf(lowerChar);
+                    if (index >= 0)
+                    {
+                        int newIndex = encrypt ?
+                            (index + shift) % alphabet.Length :
+                            (index - shift + alphabet.Length) % alphabet.Length;
+
+                        char newChar = alphabet[newIndex];
+                        result.Append(isUpper ? char.ToUpper(newChar) : newChar);
+                    }
+                    else
+                    {
+                        result.Append(c);
+                    }
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            outputTextBox.Text = result.ToString();
+        }
+    }
+}
